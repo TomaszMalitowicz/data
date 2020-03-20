81c9a575c27f4bdd692477c02cac06483a6ddc19
tom <tomasz.malitowicz@gmail.com>
2020-03-20 16:10:55 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
+const data = [
+  { id: 1, title: 'Message number 1', body: 'Message number 1 content ...' },
+  { id: 2, title: 'Message number 2', body: 'Message number 2 content ...' }
+]
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Message number ${index}`,
+    body: `Message number ${index} content ...`
+  })
+  // console.log(data)
+}, 5000)
+
 class App extends Component {
+  state = {
+    comments: [...data]
+  }
+
+  getData = () => {
+
+    if (this.state.comments.length !== data.length) {
+      console.log('actualization')
+      this.setState({
+        comments: [...data]
+      })
+    } else {
+      console.log('data are consistent, no update');
+    }
+
+  }
+  componentDidMount() {
+    this.idI = setInterval(this.getData, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.idI)
+  }
+
+
   render() {
+    const comments = this.state.comments.map(comment => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ))
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {comments.reverse()}
       </div>
     );
   }
